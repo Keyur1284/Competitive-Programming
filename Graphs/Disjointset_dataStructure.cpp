1e152b58f87e8901771dd358f5a06392cb0ce16f
Govrani Keyur <202101498@daiict.ac.in>
2022-11-10 09:41:08 +0530

    Union by Size and Rank Updated

--- Graphs/Disjointset_dataStructure.cpp
+++ Graphs/Disjointset_dataStructure.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int n = 10;
-vector <int> parent(n+1), rank(n+1);
+vector <int> parent(n+1), rank(n+1), size(n+1, 1);
 
 void makeSet()
 {
     for(int i = 1; i<=n; i++)
-        parent[i] = i, rank[i] = 0;
+        parent[i] = i, rank[i] = 0, size[i] = 1;
 }
 
 int findPar(int node)
@@ -18,7 +18,7 @@ int findPar(int node)
     return parent[node] = findPar(parent[node]);
 }
 
-void Union(int u, int v)
+void UnionRank(int u, int v)
 {
     u = findPar(u);
     v = findPar(v);
@@ -33,6 +33,27 @@ void Union(int u, int v)
     {
         parent[v] = u;
         rank[u]++;
+    }
+}
+
+void UnionSize(int u, int v)
+{
+    u = findPar(u);
+    v = findPar(v);
+
+    if(u == v)
+        return;
+
+    else if (size[u] < size[v])
+    {
+        parent[u] = v;
+        size[v] += size[u];
+    }
+
+    else if (size[u] >= size[v])
+    {
+        parent[v] = u;
+        size[u] += size[v];
     }
 }
 
