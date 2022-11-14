aa19445e7eceb94bf8e216a69c3f0d8d1402d7e9
Govrani Keyur <202101498@daiict.ac.in>
2022-11-14 17:25:16 +0530

    Permutations (Recursion)

--- Dynamic Programming/Solved Problems/46.Permutations.cpp
+++ Dynamic Programming/Solved Problems/46.Permutations.cpp
@@ -32,3 +32,32 @@ public:
         return ans;
     }
 };
+
+
+//Optimized code using least space
+class Solution {
+public:
+    
+    void Recc (int index, vector<int>& nums, vector <vector<int>> &ans)
+    {
+        if (index == nums.size())
+        {
+            ans.emplace_back(nums);
+            return;
+        }
+        
+        for (int i = index; i<nums.size(); i++)
+        {
+            swap(nums[index], nums[i]);
+            Recc(index + 1, nums, ans);
+            swap(nums[index], nums[i]);
+        }
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        
+        vector <vector<int>> ans;
+        Recc (0, nums, ans);
+        return ans;
+    }
+};
