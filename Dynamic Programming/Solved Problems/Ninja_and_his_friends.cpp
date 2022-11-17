c5c83ead6c9839230d41ca17825299f682d355c2
Govrani Keyur <202101498@daiict.ac.in>
2022-11-17 15:51:30 +0530

    Ninja's Training Solved in most optimized way

--- Dynamic Programming/Solved Problems/Ninja_and_his_friends.cpp
+++ Dynamic Programming/Solved Problems/Ninja_and_his_friends.cpp
@@ -1,4 +1,4 @@
-//Problem Link :- https://www.codingninjas.com/codestudio/problems/ninja-and-his-friends_3125885?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos
+//Problem Link :- https://www.codingninjas.com/codestudio/problems/ninja-and-his-friends_3125885
 
 //Solved by Memoization Method
 #include <bits/stdc++.h> 
