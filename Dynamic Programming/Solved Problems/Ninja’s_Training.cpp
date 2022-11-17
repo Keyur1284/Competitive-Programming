d36d8f11e10a483a0af2af4b788cf0cb76178514
Govrani Keyur <202101498@daiict.ac.in>
2022-11-17 15:51:58 +0530

    Ninja and his friends solved in most optimized way (3D dp)

--- Dynamic Programming/Solved Problems/Ninja’s_Training.cpp
+++ Dynamic Programming/Solved Problems/Ninja’s_Training.cpp
@@ -1,4 +1,4 @@
-//Problem Link :- https://www.codingninjas.com/codestudio/problems/ninja-s-training_3621003?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos
+//Problem Link :- https://www.codingninjas.com/codestudio/problems/ninja-s-training_3621003
 
 
 //Solved by Memoization Method
