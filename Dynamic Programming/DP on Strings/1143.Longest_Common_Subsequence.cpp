69df25436c155700a2ad16e0c5869bd408bdfb89
Govrani Keyur <202101498@daiict.ac.in>
2022-11-20 17:34:21 +0530

    Length of Longest Common Subsequence in two strings solved in most optimized way

---
+++ Dynamic Programming/DP on Strings/1143.Longest_Common_Subsequence.cpp
@@ -0,0 +1,137 @@
+//Problem Link :- https://leetcode.com/problems/longest-common-subsequence/
+
+//Solved by Memoization Method
+
+class Solution {
+public:
+    
+    int func (int i, int j, string &s, string &t, vector <vector <int>> &dp)
+    {
+        if (i < 0 || j < 0)
+            return 0;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        if (s[i] == t[j])
+            return dp[i][j] =  1 + func (i - 1, j - 1, s, t, dp);
+
+        else
+            return dp[i][j] = 0 + max (func (i - 1, j, s, t, dp), func (i, j - 1, s, t, dp));
+    }
+    
+    int longestCommonSubsequence(string s, string t) {
+        
+        int n = s.size();
+        int m = t.size();
+
+        vector <vector <int>> dp (n, vector <int> (m, -1));
+
+        return func (n - 1, m - 1, s, t, dp);
+        
+    }
+};
+
+
+
+//Solved by Memoization Method  (Shifting of index to right by 1)
+
+class Solution {
+public:
+    
+    int func (int i, int j, string &s, string &t, vector <vector <int>> &dp)
+    {
+        if (i == 0 || j == 0)
+            return 0;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        if (s[i - 1] == t[j - 1])
+            return dp[i][j] =  1 + func (i - 1, j - 1, s, t, dp);
+
+        else
+            return dp[i][j] = 0 + max (func (i - 1, j, s, t, dp), func (i, j - 1, s, t, dp));
+    }
+    
+    int longestCommonSubsequence(string s, string t) {
+        
+        int n = s.size();
+        int m = t.size();
+
+        vector <vector <int>> dp (n + 1, vector <int> (m + 1, -1));
+
+        return func (n, m, s, t, dp);
+        
+    }
+};
+
+
+//Solved by Tabulation Method
+
+class Solution {
+public:
+    
+    int longestCommonSubsequence(string s, string t) {
+        
+        int n = s.size();
+        int m = t.size();
+
+        vector <vector <int>> dp (n + 1, vector <int> (m + 1, -1));
+
+        for (int i = 0; i<=n; i++)
+            dp[i][0] = 0;
+
+        for (int j = 0; j<=m; j++)
+            dp[0][j] = 0;
+
+        for (int i = 1; i<=n; i++)
+        {
+            for (int j = 1; j<=m; j++)
+            {
+                if (s[i - 1] == t[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+
+                else
+                    dp[i][j] = 0 + max (dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        return dp[n][m];  
+    }
+};
+
+//Solved by Tabulation Method (Space Optimized)
+
+class Solution {
+public:
+    
+    int longestCommonSubsequence(string s, string t) {
+        
+        int n = s.size();
+        int m = t.size();
+
+        vector <int> dp(m + 1, 0);
+
+        for (int j = 0; j<=m; j++)
+            dp[j] = 0;
+
+        for (int i = 1; i<=n; i++)
+        {
+            vector <int> temp (m + 1, 0);
+
+            for (int j = 1; j<=m; j++)
+            {
+                if (s[i - 1] == t[j - 1])
+                    temp[j] = 1 + dp[j - 1];
+
+                else
+                    temp[j] = 0 + max (dp[j], temp[j - 1]);
+            }
+
+            dp = temp;
+        }
+
+        return dp[m];
+    }
+};
