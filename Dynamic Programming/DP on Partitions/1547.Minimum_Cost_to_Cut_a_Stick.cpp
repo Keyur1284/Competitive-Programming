7fe46a03f28ecdd6048ddbba38b60b6645b7b693
Govrani Keyur <202101498@daiict.ac.in>
2022-11-25 17:03:49 +0530

    Minimum cost to cut a stick solved in most optimized way

--- Dynamic Programming/DP on Partitions/1547.Minimum_Cost_to_Cut_a_Stick.cpp
+++ Dynamic Programming/DP on Partitions/1547.Minimum_Cost_to_Cut_a_Stick.cpp
@@ -44,25 +44,6 @@ public:
 class Solution {
 public:
     
-    int func (int i, int j, vector <vector <int>> &dp,vector<int>& cuts)
-    {
-        if (i > j)
-            return 0;
-        
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        
-        int mini = INT_MAX;
-        
-        for (int index = i; index <= j; index++)
-        {
-            int cost = cuts[j + 1] - cuts[i - 1] + func (i, index - 1, dp, cuts) + func (index + 1, j, dp, cuts);
-            mini = min (mini, cost);
-        }
-        
-        return dp[i][j] = mini;
-    }
-    
     int minCost(int n, vector<int>& cuts) {
         
         int size = cuts.size();
