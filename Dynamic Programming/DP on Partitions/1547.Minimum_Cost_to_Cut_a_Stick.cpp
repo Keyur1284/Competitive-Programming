3028d50bb677fa0da53d12549b9681437e91db03
Govrani Keyur <202101498@daiict.ac.in>
2022-11-25 16:36:54 +0530

    Minimum cost to cut a stick solved in most optimized way

---
+++ Dynamic Programming/DP on Partitions/1547.Minimum_Cost_to_Cut_a_Stick.cpp
@@ -0,0 +1,97 @@
+//Problem Link :- https://leetcode.com/problems/minimum-cost-to-cut-a-stick/
+
+//Solved by Memoization Method
+
+class Solution {
+public:
+    
+    int func (int i, int j, vector <vector <int>> &dp,vector<int>& cuts)
+    {
+        if (i > j)
+            return 0;
+        
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        
+        int mini = INT_MAX;
+        
+        for (int index = i; index <= j; index++)
+        {
+            int cost = cuts[j + 1] - cuts[i - 1] + func (i, index - 1, dp, cuts) + func (index + 1, j, dp, cuts);
+            mini = min (mini, cost);
+        }
+        
+        return dp[i][j] = mini;
+    }
+    
+    int minCost(int n, vector<int>& cuts) {
+        
+        int size = cuts.size();
+        cuts.emplace_back(n);
+        cuts.emplace(cuts.begin(), 0);
+        
+        sort (cuts.begin(), cuts.end());
+        
+        vector <vector <int>> dp(size + 1, vector <int> (size + 1, -1));
+        
+        return func (1, size, dp, cuts);
+    }
+};
+
+
+//Solved by Tabulation Method
+
+class Solution {
+public:
+    
+    int func (int i, int j, vector <vector <int>> &dp,vector<int>& cuts)
+    {
+        if (i > j)
+            return 0;
+        
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        
+        int mini = INT_MAX;
+        
+        for (int index = i; index <= j; index++)
+        {
+            int cost = cuts[j + 1] - cuts[i - 1] + func (i, index - 1, dp, cuts) + func (index + 1, j, dp, cuts);
+            mini = min (mini, cost);
+        }
+        
+        return dp[i][j] = mini;
+    }
+    
+    int minCost(int n, vector<int>& cuts) {
+        
+        int size = cuts.size();
+        cuts.emplace_back(n);
+        cuts.emplace(cuts.begin(), 0);
+        
+        sort (cuts.begin(), cuts.end());
+        
+        vector <vector <int>> dp(size + 2, vector <int> (size + 2, 0));
+        
+        for (int i = size; i>=1; i--)
+        {
+            for (int j = 1; j <= size; j++)
+            {
+                if (i > j)
+                    continue;
+                
+                int mini = INT_MAX;
+        
+                for (int index = i; index <= j; index++)
+                {
+                    int cost = cuts[j + 1] - cuts[i - 1] + dp[i][index - 1] + dp[index + 1][j];
+                    mini = min (mini, cost);
+                }
+
+                dp[i][j] = mini;
+            }
+        }
+        
+        return dp[1][size];
+    }
+};
