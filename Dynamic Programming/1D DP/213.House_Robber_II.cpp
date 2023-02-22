cfaefdb843395e3592dfa23237931b3f04aa431e
Govrani Keyur <202101498@daiict.ac.in>
2023-02-22 19:31:55 +0530

    added solution by tabulation method

--- Dynamic Programming/1D DP/213.House_Robber_II.cpp
+++ Dynamic Programming/1D DP/213.House_Robber_II.cpp
@@ -1,5 +1,53 @@
 //Problem Link :- https://leetcode.com/problems/house-robber-ii/
 
+// Solved by Tabulation Method
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        
+        int n = nums.size();
+
+        if (n == 1)
+            return nums[0];
+
+        if (n == 2)
+            return max(nums[0], nums[1]);
+
+        vector<int> dp1(n, 0), dp2(n, 0);
+
+        dp1[0] = nums[0];
+
+        for(int i = 1; i < n - 1; i++)
+        {
+            int take = nums[i];
+            
+            if (i > 1)
+                take += dp1[i - 2];
+
+            int notTake = 0 + dp1[i - 1];
+
+            dp1[i] = max (take, notTake);
+        }
+
+        dp2[1] = nums[1];
+
+        for(int i = 2; i < n; i++)
+        {
+            int take = nums[i];
+            
+            if (i > 2)
+                take += dp2[i - 2];
+
+            int notTake = 0 + dp2[i - 1];
+
+            dp2[i] = max (take, notTake);
+        }
+
+        return max(dp1[n - 2], dp2[n - 1]);
+    }
+};
+
 class Solution {
 public:
     int rob(vector<int>& nums) {
