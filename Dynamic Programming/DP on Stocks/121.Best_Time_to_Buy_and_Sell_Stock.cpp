0c36dcd3a6e451635f9fbce39ec3e6687d5109c9
Govrani Keyur <202101498@daiict.ac.in>
2023-02-25 14:20:26 +0530

    Best Time to Buy and Sell Stock l solved by another method

--- Dynamic Programming/DP on Stocks/121.Best_Time_to_Buy_and_Sell_Stock.cpp
+++ Dynamic Programming/DP on Stocks/121.Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -38,3 +38,27 @@ public:
         return maxProfit;
     }
 };
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int n = prices.size();
+        vector<int> maxi (n, 0);
+        
+        maxi[n - 1] = prices[n - 1];
+        int maxProfit = 0;
+
+        for (int i = n - 2; i >= 0; i--)
+        {
+            maxi[i] = max(maxi[i + 1], prices[i]);
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            maxProfit = max(maxProfit, maxi[i] - prices[i]);
+        }
+
+        return maxProfit;   
+    }
+};
