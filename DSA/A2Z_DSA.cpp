f577b9a765553db9c5642e5ca3ac6d005375f06f
Govrani Keyur <202101498@daiict.ac.in>
2023-03-07 17:23:39 +0530

    Notes updated till Lec 4

---
+++ DSA/A2Z_DSA.cpp
@@ -0,0 +1,12 @@
+// Link :- https://takeuforward.org/strivers-a2z-dsa-course/strivers-a2z-dsa-course-sheet-2/
+
+// Range of some datatypes
+-10^9 < int < 10^9;
+-10^12 < long < 10^12;
+-10^18 < long long < 10^18;
+
+// No. of operations 
+1 sec -> 10 ^8
+2 sec -> 2 * 10^8
+3 sec -> 3 * 10^8
+
