6c52a29282b5db19b9dddfa1473b240af325e256
Govrani Keyur <202101498@daiict.ac.in>
2023-03-09 22:12:04 +0530

    wrote code of bubble sort

---
+++ DSA/Sorting Techniques/Bubble_Sort.cpp
@@ -0,0 +1,44 @@
+// Description of approach
+// 1. Compare the adjacent elements
+// 2. Swap the elements if they are not in the correct order
+// 3. Repeat the above steps for the remaining array
+
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubble_sort(vector<int> &arr, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+                swap(arr[j], arr[j + 1]);
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    
+    bubble_sort(arr, n);
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    
+    return 0;
+}
