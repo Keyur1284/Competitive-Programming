3dbc39a7ce781d6d642885781a5d3e1b9d5e74d2
Govrani Keyur <202101498@daiict.ac.in>
2023-03-20 16:50:58 +0530

    solved in best optimized way

--- Solved Leetcode Problems/211. Design Add and Search Words Data Structure.cpp
+++ Solved Leetcode Problems/211. Design Add and Search Words Data Structure.cpp
@@ -1,60 +1,135 @@
 // Problem Link :- https://leetcode.com/problems/design-add-and-search-words-data-structure/
 
-// Time Complexity :- O(n)
-// Space Complexity :- O(n)
+// Time Complexity :- O(n*m)
+// Space Complexity :- O(n*m)
 
 class TrieNode {
 public:
-    TrieNode* children[26];
-    bool isWordCompleted;
 
-    TrieNode() {
-        memset(children, 0, sizeof(children));
-        isWordCompleted = false;
+    bool isEnd;
+    vector<TrieNode*> children;
+
+    TrieNode()
+    {
+        isEnd = false;
+        children.assign(26, 0);
     }
 };
 
 class WordDictionary {
 public:
+
     TrieNode* root;
+
     WordDictionary() {
+        
         root = new TrieNode();
     }
     
     void addWord(string word) {
-        TrieNode* newRoot = root;
-        for (char ch : word) {
-            int alphabetIndex = ch - 'a';
-            if (newRoot -> children[alphabetIndex] == NULL) {
-                newRoot -> children[alphabetIndex] = new TrieNode();
-            }
-            newRoot = newRoot -> children[alphabetIndex];
+
+        TrieNode* node = root;
+
+        for (auto &it : word)
+        {
+            int index = it - 'a';
+
+            if (node->children[index] == NULL)
+                node->children[index] = new TrieNode();
+
+            node = node->children[index];
         }
-        newRoot -> isWordCompleted = true;
-    }
-    
-    bool searchHelper(string word, int index, TrieNode* newRoot) {
-        if (index == word.length())
-            return newRoot -> isWordCompleted;
-        char ch = word[index];
-        if (ch == '.') {
-            for (int i = 0; i < 26; i++) {
-                if (newRoot -> children[i] != NULL && searchHelper(word, index + 1, newRoot -> children[i])) {
-                    return true;
-                }
-            }
-            return false;
-        }
-        else {
-            if (newRoot -> children[ch - 'a'] == NULL) {
-                return false;
-            }
-            return (searchHelper(word, index + 1, newRoot -> children[ch - 'a']));
-        }
+        
+        node->isEnd = true;
     }
 
+    bool searchHelper (string word, TrieNode* node)
+    {
+        int n = word.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            if (word[i] == '.')
+            {
+                for (auto &it : node->children)
+                {
+                    if (it && searchHelper(word.substr(i + 1), it))
+                        return true;
+                }
+
+                return false;
+            }
+
+            int index = word[i] - 'a';
+
+            if (node->children[index] == NULL)
+                return false;
+
+            node = node->children[index];
+        }
+
+        return node->isEnd;
+    }
+    
     bool search(string word) {
-        return searchHelper(word, 0, root);
+        
+        return searchHelper (word, root);
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
+
+
+// Solved by using vector 
+
+// Time Complexity :- O(n*m)
+// Space Complexity :- O(n*m)
+
+class WordDictionary {
+public:
+
+    vector<string> words;
+
+    WordDictionary() {
+        
+    }
+    
+    void addWord(string word) {
+        
+        words.emplace_back(word);
+    }
+    
+    bool search(string word) {
+
+        int len = word.size(), n = words.size(), count = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (words[i].size() == len)
+            {
+                for (int j = 0; j < len; j++)
+                {
+                    if (words[i][j] == word[j] || word[j] == '.')
+                        count++;
+
+                    else
+                    {
+                        count = 0;
+                        break;
+                    }
+                }
+
+                if (count == len)
+                    return true;
+            }
+        }
+
+        return false;  
     }
 };
 
