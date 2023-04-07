0a3cf7e4157f68b4c6576e0919d4729961b39338
Govrani Keyur <202101498@daiict.ac.in>
2023-04-07 23:14:08 +0530

    solved in best optimized way

--- DSA/Solved Array Problems/Medium/73. Set Matrix Zeroes.cpp
+++ DSA/Solved Array Problems/Medium/73. Set Matrix Zeroes.cpp
@@ -86,3 +86,50 @@ public:
         }   
     }
 };
+
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        
+        int m = matrix.size();
+        int n = matrix[0].size();
+        bool isRowZero = false;
+
+        for (int j = 0; j < n; j++)
+        {
+            if (matrix[0][j] == 0)
+                isRowZero = true;
+
+            for (int i = 1; i < m; i++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < m; i++)
+        {
+            for (int j = 1; j < n; j++)
+            {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            }
+        }
+
+        if (matrix[0][0] == 0)
+        {
+            for (int i = 0; i < m; i++)
+                matrix[i][0] = 0;
+        }
+
+        if (isRowZero)
+        {
+            for (int j = 0; j < n; j++)
+                matrix[0][j] = 0;
+        }   
+    }
+};
