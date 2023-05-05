ca023075f61897a3a2d7bec85c7b745d17a1ca2b
Govrani Keyur <202101498@daiict.ac.in>
2023-05-05 20:02:03 +0530

    solved in most optimized way

---
+++ Solved Leetcode Problems/1456. Maximum Number of Vowels in a Substring of Given Length.cpp
@@ -0,0 +1,68 @@
+// Problem Link :- https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/
+
+// Time Complexity :- O(n)
+// Space Complexity :- O(1)
+
+class Solution {
+public:
+    int maxVowels(string s, int k) {
+        
+        unordered_set <char> vowels;
+        vowels.emplace('a');
+        vowels.emplace('e');
+        vowels.emplace('i');
+        vowels.emplace('o');
+        vowels.emplace('u');
+
+        int temp = 0, maxi = 0;
+        int n = s.size();
+
+        for (int i = 0; i < min(k, n); i++)
+        {
+            if(vowels.find(s[i]) != vowels.end())
+                temp++;
+        }
+
+        maxi = max(maxi, temp);
+
+        for (int start = 1; start <= n - k; start++)
+        {
+            if (vowels.find(s[start - 1]) != vowels.end())
+                temp--;
+
+            if (vowels.find(s[start + k - 1]) != vowels.end())
+                temp++;
+
+            maxi = max(maxi, temp);
+        }
+
+        return maxi;
+    }
+};
+
+
+class Solution {
+public:
+    int maxVowels(string s, int k) {
+        
+        unordered_set <char> vowels {'a', 'e', 'i', 'o', 'u'};
+        
+        int temp = 0, maxi = 0;
+        int n = s.size();
+
+        for (int i = 0; i < k; i++)
+        {
+            temp += vowels.count(s[i]);
+        }
+
+        maxi = max(maxi, temp);
+
+        for (int start = k; start < n; start++)
+        {
+            temp += vowels.count(s[start]) - vowels.count(s[start - k]);
+            maxi = max(maxi, temp);
+        }
+
+        return maxi;
+    }
+};
