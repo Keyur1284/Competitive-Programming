29ba19250a9e0d6eb05e59b12a92e373f7bddfca
Govrani Keyur <202101498@daiict.ac.in>
2023-05-06 12:48:59 +0530

    solved by multiple approaches

--- Solved Leetcode Problems/1498. Number of Subsequences That Satisfy the Given Sum Condition.cpp
+++ Solved Leetcode Problems/1498. Number of Subsequences That Satisfy the Given Sum Condition.cpp
@@ -41,6 +41,65 @@ public:
 };
 
 
+// Solved by Binary Search
+
+class Solution {
+public:
+
+    int MOD = 1e9 + 7;
+
+    int binarySearch (int target, vector<int> &nums)
+    {
+        int left = 0, right = nums.size() - 1, mid, ans = left;
+
+        while (left <= right)
+        {
+            mid = (left + right) >> 1;
+
+            if (nums[mid] <= target)
+            {
+                left = mid + 1;
+                ans = left;
+            }
+
+            else
+                right = mid - 1;
+        }
+
+        return ans;
+    }
+
+    int numSubseq(vector<int>& nums, int target) {
+        
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+        vector<int> pow(n);
+        pow[0] = 1;
+
+        for (int i = 1; i < n; i++)
+        {
+            pow[i] = (pow[i - 1] * 2) % MOD;
+        }
+
+        int ans = 0;
+
+        for (int left = 0; left < n; left++)
+        {
+            int right = binarySearch(target - nums[left], nums) - 1;
+
+            if (left <= right)
+            {
+                ans += pow[right - left];
+                ans %= MOD;
+            }
+        }
+
+        return ans;
+    }
+};
+
+
 // Solved by Two pointer Approach
 
 class Solution {
@@ -74,6 +133,80 @@ public:
                 ans %= MOD;
 
                 left++;
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+// Solved by Binary Exponentiation
+
+class Solution {
+public:
+
+    int MOD = 1e9 + 7;
+
+    int mod_power (int base, int exp)
+    {
+        int res = 1;
+
+        while (exp)
+        {
+            if (exp % 2)
+            {
+                res = (1LL * res * base) % MOD;
+                exp--;
+            }
+
+            else
+            {
+                base = (1LL * base * base) % MOD;
+                exp /= 2;
+            }
+        }
+
+        return res;
+    }
+
+    int binarySearch (int target, vector<int> &nums)
+    {
+        int left = 0, right = nums.size() - 1, mid, ans = left;
+
+        while (left <= right)
+        {
+            mid = (left + right) >> 1;
+
+            if (nums[mid] <= target)
+            {
+                left = mid + 1;
+                ans = left;
+            }
+
+            else
+                right = mid - 1;
+        }
+
+        return ans;
+    }
+
+    int numSubseq(vector<int>& nums, int target) {
+        
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+
+        int ans = 0;
+
+        for (int left = 0; left < n; left++)
+        {
+            int right = binarySearch(target - nums[left], nums) - 1;
+
+            if (left <= right)
+            {
+                ans += mod_power(2, right - left);
+                ans %= MOD;
             }
         }
 
