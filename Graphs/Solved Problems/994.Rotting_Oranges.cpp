d030c317746c3af8bd59d9345f2a57f687156e59
Govrani Keyur <202101498@daiict.ac.in>
2023-05-18 21:37:22 +0530

    solved using BFS

--- Graphs/Solved Problems/994.Rotting_Oranges.cpp
+++ Graphs/Solved Problems/994.Rotting_Oranges.cpp
@@ -1,69 +1,69 @@
 //Problem Link :- https://leetcode.com/problems/rotting-oranges/
 
-struct Node {
-    int x, y, time;
-    
-    Node (int a, int b, int c)
-    {
-        x = a;
-        y = b; 
-        time = c;
-    }
-};
+// Time Complexity :- O(m*n)
+// Space Complexity :- O(m*n)
 
 class Solution {
 public:
+
+    int dx[4] = {1, -1, 0, 0}, dy[4] = {0, 0, 1, -1};
+
+    struct Node {
+
+        int x, y, time;
+
+        Node (int a, int b, int c)
+        {
+            x = a;
+            y = b; 
+            time = c;
+        }
+    };
+
     int orangesRotting(vector<vector<int>>& grid) {
         
-        queue <Node> q;
-        int row = grid.size();
-        int col = grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
         int oranges = 0;
-        
-        for (int i = 0; i<row; i++)
+
+        queue<Node> q;
+
+        for (int i = 0; i < m; i++)
         {
-            for (int j = 0; j<col; j++)
+            for (int j = 0; j < n; j++)
             {
-                if (grid[i][j] != 0)
-                    oranges++;
-                
+                oranges += (grid[i][j] > 0);
+
                 if (grid[i][j] == 2)
                     q.emplace(Node(i, j, 0));
             }
         }
-        
+
         int ans = 0, count = 0;
-        int dx[] = {-1, 0, 1, 0};
-        int dy[] = {0, -1, 0, 1};
-        
+
         while (!q.empty())
         {
             int x = q.front().x;
             int y = q.front().y;
             int time = q.front().time;
-            q.pop();
             count++;
-            
-            ans = max (ans, time);
-         
-            for (int i = 0; i<4; i++)
+            q.pop();
+
+            ans = max(ans, time);
+
+            for (int i = 0; i < 4; i++)
             {
                 int newx = x + dx[i];
                 int newy = y + dy[i];
-                
-                if (newx >= 0 && newx < row && newy >= 0 && newy < col && grid[newx][newy] == 1)
+
+                if (newx >= 0 && newx < m && newy >= 0 && newy < n && grid[newx][newy] == 1)
                 {
                     grid[newx][newy] = 2;
                     q.emplace(Node(newx, newy, time + 1));
                 }
             }
         }
-        
-        if (count == oranges)
-            return ans;
-        
-        else
-            return -1;
-        
+
+        return (count == oranges) ? ans : -1;
     }
 };
