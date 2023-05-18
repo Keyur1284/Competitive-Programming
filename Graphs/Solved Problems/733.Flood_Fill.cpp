53e206b0c070d4b1f3e6987dcd9d81451c29b233
Govrani Keyur <202101498@daiict.ac.in>
2023-05-18 18:17:09 +0530

    solved using DFS

--- Graphs/Solved Problems/733.Flood_Fill.cpp
+++ Graphs/Solved Problems/733.Flood_Fill.cpp
@@ -37,3 +37,39 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+
+    int dx[4] = {1, -1, 0, 0}, dy[4] = {0, 0, -1, 1};
+
+    void DFS (int sr, int sc, int color, int m, int n, vector<vector<bool>> &vis, vector<vector<int>>& modified, vector<vector<int>>& image)
+    {
+        vis[sr][sc] = true;
+        modified[sr][sc] = color;
+
+        for (int i = 0; i < 4; i++)
+        {
+            int newx = sr + dx[i];
+            int newy = sc + dy[i];
+
+            if (newx >= 0 && newx < m && newy >= 0 && newy < n && image[sr][sc] == image[newx][newy] && !vis[newx][newy])
+                DFS (newx, newy, color, m, n, vis, modified, image);
+        }
+    }
+
+
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        
+        int m = image.size();
+        int n = image[0].size();
+
+        vector<vector<int>> modified = image;
+        vector<vector<bool>> vis (m, vector<bool> (n, false));
+        
+        DFS (sr, sc, color, m, n, vis, modified, image);
+
+        return modified;
+    }
+};
