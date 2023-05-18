d6a1bc39c949ba0b8d790cc6b1c55543a430e22f
Govrani Keyur <202101498@daiict.ac.in>
2023-05-19 02:13:51 +0530

    solved in best optimized way using BFS

--- Graphs/Solved Problems/Distance_of_nearest_cell_having_1.cpp
+++ Graphs/Solved Problems/Distance_of_nearest_cell_having_1.cpp
@@ -99,3 +99,86 @@ int main(){
 	return 0;
 }
 // } Driver Code Ends
+
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution 
+{
+    public:
+    //Function to find distance of nearest 1 in the grid for each cell.
+    
+    int dx[4] = {1, -1, 0, 0}, dy[4] = {0, 0, 1, -1};
+    
+	vector<vector<int>>nearest(vector<vector<int>>grid)
+	{
+	    int n = grid.size();
+	    int m = grid[0].size();
+	    
+	    vector<vector<int>> ans (n, vector<int> (m, INT_MAX));
+
+	    queue<vector<int>> q;
+	    
+	    for (int i = 0; i < n; i++)
+	    {
+	        for (int j = 0; j < m; j++)
+	        {
+	            if (grid[i][j])
+	            {
+	                ans[i][j] = 0;
+	                q.push({i, j});
+	            }
+	        }
+	    }
+	    
+	    while (!q.empty())
+	    {
+	        auto it = q.front();
+	        int x = it[0], y = it[1];
+	        q.pop();
+	        
+	        for (int i = 0; i < 4; i++)
+	        {
+	            int newx = x + dx[i];
+	            int newy = y + dy[i];
+	            
+	            if (newx >= 0 && newx < n && newy >= 0 && newy < m && ans[newx][newy] > ans[x][y] + 1)
+	            {
+	                ans[newx][newy] = ans[x][y] + 1;
+	                q.push({newx, newy});
+	            }
+	        }
+	    }
+	    
+	    return ans;
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>>grid(n, vector<int>(m, -1));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				cin >> grid[i][j];
+			}
+		}
+		Solution obj;
+		vector<vector<int>> ans = obj.nearest(grid);
+		for(auto i: ans){
+			for(auto j: i){
+				cout << j << " ";
+			}
+			cout << "\n";
+		}
+	}
+	return 0;
+}
+// } Driver Code Ends
