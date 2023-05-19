61270baed9476dab1e91512cf5d27acc874e866e
Govrani Keyur <202101498@daiict.ac.in>
2023-05-19 19:36:55 +0530

    solved by DFS and BFS

--- Graphs/Solved Problems/802.Find_Eventual_Safe_States.cpp
+++ Graphs/Solved Problems/802.Find_Eventual_Safe_States.cpp
@@ -5,63 +5,59 @@
 
 class Solution {
 public:
-    
-    bool checkCycle (int node, vector <int> &vis, vector <int> &dfsVis, vector <int> &flag, vector <int> adj[])
+
+    bool checkCycle (int node, vector<bool> &vis, vector<bool> &dfsVis, vector<bool> &flag, vector<vector<int>>& graph)
     {
-        vis[node] = 1;
-        dfsVis[node] = 1;
-        flag[node] = 0;
-        
-        for (auto it : adj[node])
+        vis[node] = true;
+        dfsVis[node] = true;
+        flag[node] = false;
+
+        for (auto &it : graph[node])
         {
             if (!vis[it])
             {
-                if (checkCycle (it, vis, dfsVis, flag, adj) == true)
+                if (checkCycle(it, vis, dfsVis, flag, graph))
                 {
-                    flag[node] = 0;
+                    flag[node] = false;
                     return true;
                 }
             }
-            
-            else if (vis[it] == 1 && dfsVis[it] == 1)
+
+            else if (vis[it] && dfsVis[it])
             {
-                flag[node] = 0;
+                flag[node] = false;
                 return true;
             }
         }
-        
-        flag[node] = 1;
-        dfsVis[node] = 0;
-        
+
+        flag[node] = true;
+        dfsVis[node] = false;
+
         return false;
     }
-    
-    
+
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         
         int n = graph.size();
-        
-        vector <int> adj[n], ans, vis (n, 0), dfsVis (n, 0), flag (n, 0);
-        
-        for (int i = 0; i < n; i++)
-        {
-            for  (auto it : graph[i])
-                adj[i].emplace_back(it);
-        }
-        
+        vector<bool> vis (n, false), dfsVis (n, false), flag (n, false);
+
         for (int i = 0; i < n; i++)
         {
             if (!vis[i])
-                checkCycle(i, vis, dfsVis, flag, adj);
+            {
+                checkCycle(i, vis, dfsVis, flag, graph);
+            }
         }
-        
+
+        vector<int> safeNodes;
+
         for (int i = 0; i < n; i++)
         {
-            if (flag[i] == 1)
-                ans.emplace_back(i);
+            if (flag[i])
+                safeNodes.emplace_back(i);
         }
-        
-        return ans;
+
+        return safeNodes;
     }
 };
 
@@ -74,25 +70,19 @@ public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         
         int n = graph.size();
-
-        vector <int> adj[n], adjRev[n], indegree(n, 0), ans;
-
-        for (int i = 0; i < n; i++)
-        {
-            for (auto it : graph[i])
-                adj[i].emplace_back(it);
-        }
+        vector<int> adjRev[n], indegree(n, 0);
 
         for (int i = 0; i < n; i++)
         {
-            for (auto it : adj[i])
+            for (auto &it : graph[i])
             {
                 adjRev[it].emplace_back(i);
                 indegree[i]++;
             }
         }
 
-        queue <int> q;
+        queue<int> q;
+        vector<int> ans;
 
         for (int i = 0; i < n; i++)
         {
@@ -106,7 +96,7 @@ public:
             q.pop();
             ans.emplace_back(node);
 
-            for (auto it : adjRev[node])
+            for (auto &it : adjRev[node])
             {
                 indegree[it]--;
 
@@ -115,8 +105,8 @@ public:
             }
         }
 
-        sort (ans.begin(), ans.end());
+        sort(ans.begin(), ans.end());
 
-        return ans;
+        return ans; 
     }
 };
