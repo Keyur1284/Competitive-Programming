1acf746caa0bc84b2059402c42f100cb2ff41707
Govrani Keyur <202101498@daiict.ac.in>
2023-05-31 20:06:56 +0530

    Solved by Morris Traversal Algorithm

--- Trees/Solved Problems/144. Binary Tree Preorder Traversal.cpp
+++ Trees/Solved Problems/144. Binary Tree Preorder Traversal.cpp
@@ -41,6 +41,64 @@ public:
 };
 
 
+
+// Solved by Morris Preorder Traversal Algorithm
+// Time Complexity :- O(N)
+// Space Complexity :- O(1)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        
+        vector<int> preorder;
+        TreeNode* curr = root;
+
+        while (curr)
+        {
+            if (curr->left == NULL)
+            {
+                preorder.emplace_back(curr->val);
+                curr = curr->right;
+            }
+
+            else
+            {
+                TreeNode* prev = curr->left;
+
+                while (prev->right && prev->right != curr)
+                    prev = prev->right;
+
+                if (prev->right == NULL)
+                {
+                    prev->right = curr;
+                    preorder.emplace_back(curr->val);
+                    curr = curr->left;
+                }
+
+                else if (prev->right == curr)
+                {
+                    prev->right = NULL;
+                    curr = curr->right;
+                }
+            }
+        }
+
+        return preorder;
+    }
+};
+
+
 // Solved by Iterative Approach
 
 /**
--- Trees/Solved Problems/94. Binary Tree Inorder Traversal.cpp
+++ Trees/Solved Problems/94. Binary Tree Inorder Traversal.cpp
@@ -40,6 +40,66 @@ public:
 };
 
 
+// Solved by Morris Inorder Traversal Algorithm
+// Time Complexity :- O(N)
+// Space Complexity :- O(1)
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        
+        vector<int> inorder;
+        TreeNode* curr = root;
+
+        while (curr)
+        {
+            if (curr->left == NULL)
+            {
+                inorder.emplace_back(curr->val);
+                curr = curr->right;
+            }
+
+            else
+            {
+                TreeNode* prev = curr->left;
+
+                while (prev->right && prev->right != curr)
+                {
+                    prev = prev->right;
+                }
+                    
+                if (prev->right == NULL)
+                {
+                    prev->right = curr;
+                    curr = curr->left;
+                }
+
+                else if (prev->right == curr)
+                {
+                    prev->right = NULL;
+                    inorder.emplace_back(curr->val);
+                    curr = curr->right;
+                }
+            }
+        }
+
+        return inorder;
+    }
+};
+
+
 // Solved by Iterative Approach
 
 /**
