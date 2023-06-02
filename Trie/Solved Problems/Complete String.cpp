a7c7423bc0acdda29dd3293e20bf7d76193e043f
Govrani Keyur <202101498@daiict.ac.in>
2023-06-02 21:25:06 +0530

    Solved by Advanced Trie Data Structure

---
+++ Trie/Solved Problems/Complete String.cpp
@@ -0,0 +1,84 @@
+// Problem Link :- https://www.codingninjas.com/codestudio/problems/complete-string_2687860
+
+// Solved by Advanced Trie Data Structure
+
+// Time Complexity :- O(N * M)      N -> No. of words, M -> Length of longest word
+// Space Complexity :- O(N)
+
+#include <bits/stdc++.h> 
+
+class TrieNode {
+
+    public:
+
+    int countWords;
+    vector<TrieNode*> child;
+
+    TrieNode ()
+    {
+        countWords = 0;
+        child.assign (26, NULL);
+    }
+};
+
+TrieNode* root;
+
+void insert(string &word){
+        
+    TrieNode* node = root;
+
+    for (auto &it : word)
+    {
+        int index = it - 'a';
+
+        if (node->child[index] == NULL)
+            node->child[index] = new TrieNode();
+
+        node = node->child[index];
+    }
+
+    node->countWords++;
+}
+
+bool search (string &word)
+{
+    TrieNode* node = root;
+
+    for (auto &it : word)
+    {
+        int index = it - 'a';
+
+        node = node->child[index];
+
+        if (node->countWords == 0)
+            return false;
+    }
+
+    return true;
+}
+
+string completeString(int n, vector<string> &a){
+    
+    root = new TrieNode();
+
+    for (auto &str : a)
+    {
+        insert(str);
+    }
+
+    string ans = "";
+
+    for (auto &str : a)
+    {
+        if (search(str))
+        {
+            if (ans.length() < str.length())
+                ans = str;
+
+            else if (ans.length() == str.length())
+                ans = min(ans, str);
+        }
+    }
+
+    return (ans.size()) ? ans : "None";
+}
