282a0e48da359bca02eab6569ed63088476a5908
Govrani Keyur <202101498@daiict.ac.in>
2023-06-02 21:32:36 +0530

    Solved by Trie and Advanced Trie Data Structure

--- Trie/Solved Problems/Complete String.cpp
+++ Trie/Solved Problems/Complete String.cpp
@@ -82,3 +82,85 @@ string completeString(int n, vector<string> &a){
 
     return (ans.size()) ? ans : "None";
 }
+
+
+
+// Solved by Trie Data Structure
+
+#include <bits/stdc++.h> 
+
+class TrieNode {
+
+    public:
+
+    bool isEnd;
+    vector<TrieNode*> child;
+
+    TrieNode ()
+    {
+        isEnd = false;
+        child.assign (26, NULL);
+    }
+};
+
+TrieNode* root;
+
+void insert(string &word){
+        
+    TrieNode* node = root;
+
+    for (auto &it : word)
+    {
+        int index = it - 'a';
+
+        if (node->child[index] == NULL)
+            node->child[index] = new TrieNode();
+
+        node = node->child[index];
+    }
+
+    node->isEnd = true;
+}
+
+bool search (string &word)
+{
+    TrieNode* node = root;
+
+    for (auto &it : word)
+    {
+        int index = it - 'a';
+
+        node = node->child[index];
+
+        if (node->isEnd == false)
+            return false;
+    }
+
+    return true;
+}
+
+string completeString(int n, vector<string> &a){
+    
+    root = new TrieNode();
+
+    for (auto &str : a)
+    {
+        insert(str);
+    }
+
+    string ans = "";
+
+    for (auto &str : a)
+    {
+        if (search(str))
+        {
+            if (ans.length() < str.length())
+                ans = str;
+
+            else if (ans.length() == str.length())
+                ans = min(ans, str);
+        }
+    }
+
+    return (ans.size()) ? ans : "None";
+}
