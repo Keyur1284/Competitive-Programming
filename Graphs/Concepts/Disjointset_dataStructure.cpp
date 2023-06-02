52a5d9bc1805363ce8e0f0692cf59a9c3731a29f
Govrani Keyur <202101498@daiict.ac.in>
2023-06-02 13:46:17 +0530

    added dsu template

--- Graphs/Concepts/Disjointset_dataStructure.cpp
+++ Graphs/Concepts/Disjointset_dataStructure.cpp
@@ -80,4 +80,83 @@ int main()
         cout << "Same" << endl;
 }
 
+
+// DSU template
+
+class DSU {
+
+private:
     
+    vector<int> parent, rank, size;
+
+public:
+
+    DSU (int n)
+    {
+        for (int i = 0; i <= n; i++)
+        {
+            parent.emplace_back(i);
+            rank.emplace_back(0);
+            size.emplace_back(1);
+        }
+    }
+
+    int findPar (int node)
+    {
+        if (node == parent[node])
+            return node;
+
+        return parent[node] = findPar(parent[node]);
+    }
+
+    void UnionRank (int u, int v)
+    {
+        u = findPar(u);
+        v = findPar(v);
+
+        if (u == v)
+            return;
+
+        if (rank[u] < rank[v])
+        {
+            parent[u] = v;
+        }
+
+        else if (rank[u] > rank[v])
+        {
+            parent[v] = u;
+        }
+
+        else if (rank[u] == rank[v])
+        {
+            parent[v] = u;
+            rank[u]++;
+        }
+    }
+
+    void UnionSize (int u, int v)
+    {
+        u = findPar(u);
+        v = findPar(v);
+
+        if (u == v)
+            return;
+
+        if (size[u] < size[v])
+        {
+            parent[u] = v;
+            size[v] += size[u];
+        }
+
+        else if (rank[u] >= rank[v])
+        {
+            parent[v] = u;
+            size[u] += size[v];
+        }
+    }   
+    
+    int getSize (int u)
+    {
+        return size[u];
+    }
+};
