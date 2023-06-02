4911ba40affe2ffe8bd7ef081a42f15056b0c9aa
Govrani Keyur <202101498@daiict.ac.in>
2023-06-02 22:12:14 +0530

    Solved by Trie Data Structure

---
+++ Trie/Solved Problems/Count Distinct Substrings.cpp
@@ -0,0 +1,119 @@
+// Problem Link :- https://www.codingninjas.com/codestudio/problems/count-distinct-substrings_985292
+
+// Solved by Trie Data Structure
+// Time Complexity :- O(N^2)
+// Space Complexity :- O(N^2)
+
+class TrieNode {
+
+    public:
+
+    vector<TrieNode*> child;
+
+    TrieNode ()
+    {
+        child.assign(26, NULL);
+    }
+};
+
+TrieNode *root;
+int count;
+
+void insert (string &word)
+{
+    TrieNode *node = root;
+
+    for (auto &it : word)
+    {
+        int index = it - 'a';
+
+        if (node->child[index] == NULL)
+        {
+            node->child[index] = new TrieNode();
+            count++;
+        }
+
+        node = node->child[index];
+    }
+}
+
+int countDistinctSubstrings(string &s)
+{
+    root = new TrieNode();
+    count = 1;
+
+    int len = s.length();
+
+    for (int i = 0; i < len; i++)
+    {
+        string word = s.substr(i);
+        insert(word);
+    }
+
+    return count;
+}
+
+
+// Solution by using concepts of OOPs
+
+class TrieNode {
+
+    public:
+
+    vector<TrieNode*> child;
+
+    TrieNode ()
+    {
+        child.assign(26, NULL);
+    }
+};
+
+int count;
+
+class Trie {
+
+    public:
+    
+    TrieNode *root;
+
+    Trie() {
+        
+        root = new TrieNode();
+    }
+
+    void insert (string &word)
+    {
+        TrieNode *node = root;
+
+        for (auto &it : word)
+        {
+            int index = it - 'a';
+
+            if (node->child[index] == NULL)
+            {
+                node->child[index] = new TrieNode();
+                count++;
+            }
+
+            node = node->child[index];
+        }
+    }
+
+};
+
+
+int countDistinctSubstrings(string &s)
+{
+    Trie *obj = new Trie();
+    count = 1;
+
+    int len = s.length();
+
+    for (int i = 0; i < len; i++)
+    {
+        string word = s.substr(i);
+        obj->insert(word);
+    }
+
+    return count;
+}
