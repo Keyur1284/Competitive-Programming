9c1f3633f0603c55e1c6a3a6d10aa189d014c5c3
Govrani Keyur <202101498@daiict.ac.in>
2023-06-03 16:50:14 +0530

    Solved by Iterative Binary Searching and inbuilt upper_bound func

---
+++ Binary Search/Solved Problems/Floor in a Sorted Array.cpp
@@ -0,0 +1,122 @@
+// Problem Link :- https://practice.geeksforgeeks.org/problems/floor-in-a-sorted-array-1587115620/1
+
+// Time Complexity :- O(log_2 n)
+// Space Complexity :- O(1)
+
+// Solved by Iterative Binary Search
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+  public:
+    // Function to find floor of x
+    // n: size of vector
+    // x: element whose floor is to find
+    int findFloor(vector<long long> v, long long n, long long x){
+        
+        long long low = 0, high = n - 1, ans = -1, mid;
+        
+        while (low <= high)
+        {
+            mid = (low + high) >> 1;
+            
+            if (v[mid] <= x)
+            {
+                ans = mid;
+                low = mid + 1;
+            }
+            
+            else
+                high = mid - 1;
+        }
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	long long t;
+	cin >> t;
+	
+	while(t--){
+	    long long n;
+	    cin >> n;
+	    long long x;
+	    cin >> x;
+	    
+	    vector<long long> v;
+	    
+	    for(long long i = 0;i<n;i++){
+	        long long temp;
+	        cin >> temp;
+	        v.push_back(temp);
+	    }
+	    Solution obj;
+	    cout << obj.findFloor(v, n, x) << endl;
+	   
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
+
+
+// Solved by inbuilt upper_bound function
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+  public:
+    // Function to find floor of x
+    // n: size of vector
+    // x: element whose floor is to find
+    int findFloor(vector<long long> v, long long n, long long x){
+        
+        int index = upper_bound (v.begin(), v.end(), x) - v.begin();
+        
+        return (index > 0 && v[index - 1] <= x) ? index - 1 : -1;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	long long t;
+	cin >> t;
+	
+	while(t--){
+	    long long n;
+	    cin >> n;
+	    long long x;
+	    cin >> x;
+	    
+	    vector<long long> v;
+	    
+	    for(long long i = 0;i<n;i++){
+	        long long temp;
+	        cin >> temp;
+	        v.push_back(temp);
+	    }
+	    Solution obj;
+	    cout << obj.findFloor(v, n, x) << endl;
+	   
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
