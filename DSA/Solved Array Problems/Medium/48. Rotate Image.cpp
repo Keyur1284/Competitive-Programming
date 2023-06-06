32aa0a7cedf589699b983a849c2d1bb7af40b792
Govrani Keyur <202101498@daiict.ac.in>
2023-06-06 15:35:47 +0530

    Also added code for anti-clockwise rotation

--- DSA/Solved Array Problems/Medium/48. Rotate Image.cpp
+++ DSA/Solved Array Problems/Medium/48. Rotate Image.cpp
@@ -3,6 +3,7 @@
 // Time Complexity :- O(n^2)
 // Space Complexity :- O(n^2)
 
+// Clockwise Rotation
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
@@ -33,13 +34,55 @@ public:
         
         int n = matrix.size();
 
-        vector<vector<int>> grid (n, vector<int> (n));
-
         for (int i = 0; i < n - 1; i++)
         {
             for (int j = i + 1; j < n; j++)
             {
                 swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+            reverse(matrix[i].begin(), matrix[i].end());
+    }
+};
+
+
+
+// Anti-Clockwise Rotation
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        
+        int n = matrix.size();
+
+        vector<vector<int>> grid (n, vector<int> (n));
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                grid[n - 1 - j][i] = matrix[i][j];
+            }
+        }
+
+        matrix = grid;
+    }
+};
+
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        
+        int n = matrix.size();
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = n - 1; j >= i + 1; j--)
+            {
+                swap(matrix[i][n - 1 - j], matrix[j][n - 1 - i]);
             }
         }
 
