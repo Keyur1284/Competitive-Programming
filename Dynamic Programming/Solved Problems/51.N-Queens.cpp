f3674db02c11a5ca3aeff99d61a7a856f42d8280
Govrani Keyur <202101498@daiict.ac.in>
2023-06-13 17:32:34 +0530

    fixed errors

--- Dynamic Programming/Solved Problems/51.N-Queens.cpp
+++ Dynamic Programming/Solved Problems/51.N-Queens.cpp
@@ -94,17 +94,17 @@ public:
         
         for (int row = 0; row < n; row++)
         {
-            if (leftRow[row] == 0 && upperLeftDiagonal[row + col] == 0 && lowerLeftDiagonal[n - 1 + col - row] == 0)
+            if (leftRow[row] == 0 && lowerLeftDiagonal[row + col] == 0 && upperLeftDiagonal[n - 1 + col - row] == 0)
             {
                 board[row][col] = 'Q';
                 leftRow[row] = 1;
-                upperLeftDiagonal[row + col] = 1;
-                lowerLeftDiagonal[n - 1 + col - row] = 1;
+                lowerLeftDiagonal[row + col] = 1;
+                upperLeftDiagonal[n - 1 + col - row] = 1;
                 solve (col + 1, board, ans, leftRow, upperLeftDiagonal, lowerLeftDiagonal, n);
                 board[row][col] = '.';
                 leftRow[row] = 0;
-                upperLeftDiagonal[row + col] = 0;
-                lowerLeftDiagonal[n - 1 + col - row] = 0;
+                lowerLeftDiagonal[row + col] = 0;
+                upperLeftDiagonal[n - 1 + col - row] = 0;
             }
         }
     }
