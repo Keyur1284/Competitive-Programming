09edf5f3dcccbe5aa1d5a34ccc877dcadabd5ba4
Govrani Keyur <202101498@daiict.ac.in>
2023-07-05 11:45:35 +0530

    Solved in best optimized way

--- Dynamic Programming/DP on Stocks/122.Best_Time_to_Buy_and_Sell_Stock_II.cpp
+++ Dynamic Programming/DP on Stocks/122.Best_Time_to_Buy_and_Sell_Stock_II.cpp
@@ -1,5 +1,24 @@
 //Problem Link :- https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
 
+// Solved by greedy approach
+
+class Solution {
+public:
+
+    int maxProfit(vector<int>& prices) {
+        
+        int n = prices.size(), profit = 0;
+
+        for (int i = 1; i < n; i++)
+        {
+            if (prices[i] > prices[i - 1])
+                profit += prices[i] - prices[i - 1];
+        }
+
+        return profit;
+    }
+};
+
 //Solved by Memoization Method
 
 class Solution {
