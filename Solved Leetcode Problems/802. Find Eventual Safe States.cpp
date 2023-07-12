27a63188c7916eef036afd9b969e50a9c592ec93
Govrani Keyur <202101498@daiict.ac.in>
2023-07-12 13:01:05 +0530

    Solved by BFS and DFS

---
+++ Solved Leetcode Problems/802. Find Eventual Safe States.cpp
@@ -0,0 +1,115 @@
+// Problem Link :- https://leetcode.com/problems/find-eventual-safe-states/
+
+// Solved by Reversing Graph and using Kahn's Algorithm (Toposort)
+// Time Complexity :- O(V + E)
+// Space Complexity :- O(V + E)
+
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        
+        int n = graph.size();
+        vector<int> adjRev[n], indegree(n, 0);
+
+        for (int i = 0; i < n; i++)
+        {
+            for (auto &it : graph[i])
+            {
+                adjRev[it].emplace_back(i);
+                indegree[i]++;
+            }
+        }
+
+        queue<int> q;
+        vector<int> ans;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (indegree[i] == 0)
+                q.emplace(i);
+        }
+
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            ans.emplace_back(node);
+
+            for (auto &it : adjRev[node])
+            {
+                indegree[it]--;
+
+                if (indegree[it] == 0)
+                    q.emplace(it);
+            }
+        }
+
+        sort(ans.begin(), ans.end());
+
+        return ans; 
+    }
+};
+
+
+
+// Solved by checking nodes leading to form cycle (DFS)
+// Time Complexity :- O(V + E)
+// Space Complexity :- O(V + E)
+
+class Solution {
+public:
+
+    bool checkCycle (int node, vector<bool> &vis, vector<bool> &dfsVis, vector<bool> &flag, vector<vector<int>>& graph)
+    {
+        vis[node] = true;
+        dfsVis[node] = true;
+        flag[node] = false;
+
+        for (auto &it : graph[node])
+        {
+            if (!vis[it])
+            {
+                if (checkCycle(it, vis, dfsVis, flag, graph))
+                {
+                    flag[node] = false;
+                    return true;
+                }
+            }
+
+            else if (vis[it] && dfsVis[it])
+            {
+                flag[node] = false;
+                return true;
+            }
+        }
+
+        flag[node] = true;
+        dfsVis[node] = false;
+
+        return false;
+    }
+
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        
+        int n = graph.size();
+        vector<bool> vis (n, false), dfsVis (n, false), flag (n, false);
+
+        for (int i = 0; i < n; i++)
+        {
+            if (!vis[i])
+            {
+                checkCycle(i, vis, dfsVis, flag, graph);
+            }
+        }
+
+        vector<int> safeNodes;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (flag[i])
+                safeNodes.emplace_back(i);
+        }
+
+        return safeNodes;
+    }
+};
