e32f4e317087bfb90254dcd29eb95260727b3d24
Govrani Keyur <202101498@daiict.ac.in>
2023-08-16 10:51:09 +0530

    Solved using deque

---
+++ Solved Leetcode Problems/239. Sliding Window Maximum.cpp
@@ -0,0 +1,31 @@
+// Problem Link :- https://leetcode.com/problems/sliding-window-maximum/
+
+// Solved using deque
+// Time Complexity :- O(n)
+// Space Complexity :- O(k)
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        deque<int> dq;
+        vector<int> ans;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (!dq.empty() && dq.front() == i - k)
+                dq.pop_front();
+
+            while (!dq.empty() && nums[dq.back()] <= nums[i])
+                dq.pop_back();
+
+            dq.push_back(i);
+
+            if (i >= k - 1)
+                ans.emplace_back(nums[dq.front()]);
+        }
+
+        return ans;
+    }
+};
