c39c1fa7b40616846f6cb88dca21c0607f0ca616
Govrani Keyur <202101498@daiict.ac.in>
2023-12-05 19:06:06 +0530

    Solved in best optimized way

---
+++ Solved Leetcode Problems/1688. Count of Matches in Tournament.cpp
@@ -0,0 +1,56 @@
+// Problem Link :- https://leetcode.com/problems/count-of-matches-in-tournament/
+
+// Solved by Simulating
+// Time Complexity :- O(logn)
+// Space Complexity :- O(1)
+
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        
+        int matches = 0;
+
+        while (n != 1)
+        {
+            matches += n/2;
+            n = n/2 + (n & 1);
+        }
+
+        return matches;
+    }
+};
+
+
+
+// Solved by Recursion
+// Time Complexity :- O(logn)
+// Space Complexity :- O(logn)
+
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        
+        if (n == 1)
+            return 0;
+        
+        if (n & 1)
+            return n/2 + numberOfMatches(n/2 + 1);
+
+        else
+            return n/2 + numberOfMatches(n/2);
+    }
+};
+
+
+
+// Solved by Logic
+// Time Complexity :- O(1)
+// Space Complexity :- O(1)
+
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        
+        return n - 1;
+    }
+};
