a875564186e2c045164624083da1fda679fc52e9
Govrani Keyur <202101498@daiict.ac.in>
2024-01-10 22:05:48 +0530

    Add Solution done by Tabulation Method

--- Leetcode POTD/2023/486. Predict the Winner.cpp
+++ Leetcode POTD/2023/486. Predict the Winner.cpp
@@ -1,6 +1,6 @@
 // Problem Link :- https://leetcode.com/problems/predict-the-winner/
 
-// Solved by DP
+// Solved by Memoization Method
 // Time Complexity :- O(n^2)
 // Space Complexity :- O(n^2)
 
@@ -68,5 +68,34 @@ public:
         vector<vector<int>> dp(n, vector<int> (n, -1));
 
         return (solve(0, n - 1, dp, nums) >= 0);
+    }
+};
+
+
+
+// Solved by Tabulation Method
+// Time Complexity :- O(n^2)
+// Space Complexity :- O(n^2)
+
+class Solution {
+public:
+    bool predictTheWinner(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int> (n + 1, 0));
+
+        for (int i = 0; i < n; i++)
+            dp[i][i] = nums[i];
+
+        for (int diff = 1; diff < n; diff++)
+        {
+            for (int left = 0; left < n - diff; left++)
+            {
+                int right = left + diff;
+                dp[left][right] = max(nums[left] - dp[left + 1][right], nums[right] - dp[left][right - 1]);
+            }
+        }
+
+        return (dp[0][n - 1] >= 0);
     }
 };
