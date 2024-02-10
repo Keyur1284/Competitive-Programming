430b3368a1d7c5fd96923ce765e74d82fff63e2f
Govrani Keyur <202101498@daiict.ac.in>
2024-02-10 14:44:53 +0530

    Solved by DP and Manacher's Algorithm

--- Leetcode POTD/2024/647. Palindromic Substrings.cpp
+++ Leetcode POTD/2024/647. Palindromic Substrings.cpp
@@ -73,6 +73,32 @@ public:
 
 
 
+class Solution {
+public:
+    int countSubstrings(string s) {
+        
+        int n = s.length();
+        vector<vector<int>> dp(n, vector<int> (n, 0));
+
+        int count = 0;
+
+        for (int left = n - 1; left >= 0; left--)
+        {
+            for (int right = left; right < n; right++)
+            {
+                if (s[left] == s[right] && (right - left <= 1 || dp[left + 1][right - 1]))
+                    dp[left][right] = 1;
+
+                count += dp[left][right];
+            }
+        }
+
+        return count;
+    }
+};
+
+
+
 // Solved by Expand Around Center Method
 // Time Complexity :- O(n^2)
 // Space Complexity :- O(1)
@@ -102,6 +128,59 @@ public:
             count++;
             left--;
             right++;
+        }
+
+        return count;
+    }
+};
+
+
+
+// Solved by Manacher's Algorithm
+// Time Complexity :- O(n)
+// Space Complexity :- O(n)
+
+class Solution {
+public:
+    int countSubstrings(string s) {
+        
+        int n = s.size();
+        string st = string(2 * n + 1, '!');
+
+        for (int i = 0; i < n; i++)
+        {
+            st[2 * i + 1] = s[i];
+        }
+
+        vector<int> pal(2 * n + 1, 0);
+        int center = 0, right = 0, count = 0;
+
+        for (int i = 0; i < 2 * n + 1; i++)
+        {
+            int mirror = 2 * center - i;
+
+            if (right - i > 0)
+            {
+                pal[i] = min(right - i, pal[mirror]);
+            }
+
+            int l = i - pal[i] - 1;
+            int r = i + pal[i] + 1;
+
+            while (l >= 0 && r < 2 * n + 1 && st[l] == st[r])
+            {
+                l--;
+                r++;
+                pal[i]++;
+            }
+
+            if (i + pal[i] > right)
+            {
+                center = i;
+                right = i + pal[i];
+            }
+
+            count += (pal[i] + 1)/2;
         }
 
         return count;
