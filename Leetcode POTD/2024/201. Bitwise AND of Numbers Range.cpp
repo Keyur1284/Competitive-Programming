e2d5384707c112284845b1e7e90d48ae7dc59504
Govrani Keyur <202101498@daiict.ac.in>
2024-02-21 08:18:56 +0530

    Solved by bit manipulation

---
+++ Leetcode POTD/2024/201. Bitwise AND of Numbers Range.cpp
@@ -0,0 +1,40 @@
+// Problem Link :- https://leetcode.com/problems/bitwise-and-of-numbers-range/
+
+// Solved by Bit Manipulation
+// Time Complexity :- O(32)
+// Space Complexity :- O(1)
+
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        
+        int mask = 1 << 30, andVal = 0;
+
+        while (mask && (left & mask) == (right & mask))
+        {
+            andVal |= (left & mask);
+            mask >>= 1;
+        }
+
+        return andVal;
+    }
+};
+
+
+
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        
+        int bit = 0;
+
+        while (left != right)
+        {
+            left >>= 1;
+            right >>= 1;
+            bit++;
+        }
+
+        return (left << bit);
+    }
+};
