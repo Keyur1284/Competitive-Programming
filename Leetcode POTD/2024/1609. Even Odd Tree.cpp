d4bf1c5ecc8674b43347399738bf2b670d955a15
Govrani Keyur <202101498@daiict.ac.in>
2024-02-29 12:21:37 +0530

    Solved by BFS and DFS

---
+++ Leetcode POTD/2024/1609. Even Odd Tree.cpp
@@ -0,0 +1,123 @@
+// Problem Link :- https://leetcode.com/problems/even-odd-tree/
+
+// Solved by BFS (Level Order Traversal)
+// Time Complexity :- O(n)
+// Space Complexity :- O(n)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isEvenOddTree(TreeNode* root) {
+        
+        queue<TreeNode*> BFS;
+        BFS.emplace(root);
+        int level = 0;
+
+        while (!BFS.empty())
+        {
+            int size = BFS.size();
+            int prev = -1;
+
+            while (size--)
+            {
+                auto node = BFS.front();
+                BFS.pop();
+
+                if (level & 1)
+                {
+                    if (node->val % 2 != 0)
+                        return false;
+
+                    else
+                    {
+                        if (prev == -1 || prev > node->val)
+                            prev = node->val;
+
+                        else
+                            return false;
+                    }
+                }
+
+                else
+                {
+                    if (node->val % 2 != 1)
+                        return false;
+
+                    else
+                    {
+                        if (prev == -1 || prev < node->val)
+                            prev = node->val;
+
+                        else
+                            return false;
+                    }
+                }
+
+                if (node->left)
+                    BFS.emplace(node->left);
+
+                if (node->right)
+                    BFS.emplace(node->right);
+            }
+
+            level++;
+        }
+
+        return true;
+    }
+};
+
+
+
+// Solved by DFS
+// Time Complexity :- O(n)
+// Space Complexity :- O(n)
+
+class Solution {
+public:
+
+    vector<int> prev;
+
+    bool DFS(TreeNode* node, int level) 
+    {
+        if (node == NULL)
+            return true;
+
+        if (node->val % 2 == level % 2) 
+            return false;
+
+        if (prev.size() == level)
+            prev.push_back(0);
+
+        if (prev[level] != 0)
+        {
+            if (level % 2 == 0 && node->val <= prev[level])
+                return false;
+
+            if (level % 2 == 1 && node->val >= prev[level])
+                return false;
+        }
+
+        prev[level] = node->val;
+
+        bool left = DFS(node->left, level + 1);
+        bool right = DFS(node->right, level + 1);
+
+        return (left && right);
+    }
+
+    bool isEvenOddTree(TreeNode* root) {
+
+        return DFS(root, 0);
+    }
+};
