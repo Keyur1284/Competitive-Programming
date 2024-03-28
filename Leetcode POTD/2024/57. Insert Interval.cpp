f95bc23b2a6e0eed5c5c1e2486814add93e6937f
Govrani Keyur <202101498@daiict.ac.in>
2024-03-28 12:56:55 +0530

    Solved in best optimized way

--- Leetcode POTD/2024/57. Insert Interval.cpp
+++ Leetcode POTD/2024/57. Insert Interval.cpp
@@ -54,3 +54,74 @@ public:
         
     }
 };
+
+
+
+class Solution {
+public:
+
+    void insertion (vector<vector<int>>& intervals, vector<int>& newInterval)
+    {
+        auto index = upper_bound (intervals.begin(), intervals.end(), newInterval) - intervals.begin();
+
+        if (index == intervals.size())
+            intervals.emplace_back(newInterval);
+
+        else
+            intervals.insert(intervals.begin() + index, newInterval);
+    }
+
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        
+        insertion (intervals, newInterval);
+
+        vector<vector<int>> answer;
+        
+        for (auto &interval : intervals) 
+        {
+            if (answer.empty() || answer.back()[1] < interval[0]) 
+                answer.push_back(interval);
+
+            else 
+                answer.back()[1] = max(answer.back()[1], interval[1]);
+        }
+
+        return answer;
+    }
+};
+
+
+
+// Solved by Linear Search
+// Time Complexity :- O(n)
+// Space Complexity :- O(n)
+
+class Solution {
+public:
+   vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        
+        int n = intervals.size(), i = 0;
+        vector<vector<int>> finalIntervals;
+
+        while (i < n && intervals[i][1] < newInterval[0]) 
+        {
+            finalIntervals.emplace_back(intervals[i++]);
+        }    
+        
+        while (i < n && newInterval[1] >= intervals[i][0]) 
+        {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        
+        finalIntervals.emplace_back(newInterval);
+        
+        while (i < n)
+        {
+            finalIntervals.emplace_back(intervals[i++]);
+        }
+
+        return finalIntervals;
+    }
+};
