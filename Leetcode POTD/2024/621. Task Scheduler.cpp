1a8cfafacee090e19e5688bbb2d7cab7d34e6d00
Govrani Keyur <202101498@daiict.ac.in>
2024-03-28 15:09:27 +0530

    Solved in best optimized way

--- Leetcode POTD/2024/621. Task Scheduler.cpp
+++ Leetcode POTD/2024/621. Task Scheduler.cpp
@@ -48,3 +48,102 @@ public:
         return time;
     }
 };
+
+
+
+// Solved by filling the idle slots
+// Time Complexity :- O(n + 26 * log(26))
+// Space Complexity :- O(26)
+
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+
+        vector<int> freq(26, 0);
+
+        for (auto &ch : tasks) 
+            freq[ch - 'A']++;
+
+        sort(freq.begin(), freq.end());
+
+        int maxFreq = freq[25] - 1;
+        int idleSlots = maxFreq * n;
+
+        for (int i = 24; i >= 0 && freq[i] > 0; i--) 
+        {
+            idleSlots -= min(maxFreq, freq[i]);
+        }
+
+        return idleSlots > 0 ? idleSlots + tasks.size() : tasks.size();
+    }
+};
+
+
+
+// Solved by Greedy Approach
+// Time Complexity :- O(n)
+// Space Complexity :- O(26)
+
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+
+        vector<int> freq(26, 0);
+        int maximum = 0, maxCount = 0;
+
+        for (auto &ch : tasks) 
+        {
+            freq[ch - 'A']++;
+            
+            if (freq[ch - 'A'] > maximum) 
+            {
+                maximum = freq[ch - 'A'];
+                maxCount = 1;
+            } 
+
+            else if (freq[ch - 'A'] == maximum) 
+                maxCount++;
+        }
+        
+        int partCount = maximum - 1;
+        int partLength = n - (maxCount - 1);
+        int emptySlots = partCount * partLength;
+        int availableTasks = tasks.size() - maximum * maxCount;
+        int idles = max(0, emptySlots - availableTasks);
+        
+        return tasks.size() + idles;
+    }
+};
+
+
+
+// Solved by Math
+// Time Complexity :- O(n)
+// Space Complexity :- O(26)
+
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+
+        vector<int> freq(26, 0);
+        int maximum = 0, maxCount = 0;
+
+        for (auto &ch : tasks) 
+        {
+            freq[ch - 'A']++;
+            
+            if (freq[ch - 'A'] > maximum) 
+            {
+                maximum = freq[ch - 'A'];
+                maxCount = 1;
+            } 
+
+            else if (freq[ch - 'A'] == maximum) 
+                maxCount++;
+        }
+        
+        int time = (maximum - 1) * (n + 1) + maxCount;
+        
+        return max((int)tasks.size(), time);
+    }
+};
