ccdd9b4d7b1cf82e1b2d6750d59491f9535e71cc
Govrani Keyur <202101498@daiict.ac.in>
2024-05-11 12:59:02 +0530

    Solved using priority_queue and sorting

---
+++ Leetcode POTD/2024/857. Minimum Cost to Hire K Workers.cpp
@@ -0,0 +1,43 @@
+// Problem Link :- https://leetcode.com/problems/minimum-cost-to-hire-k-workers/
+
+// Solved using priority_queue and sorting
+// Time Complexity :- O(n * (logn + logk))
+// Space Complexity :- O(n + k)
+
+class Solution {
+public:
+    double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k) {
+        
+        int n = wage.size();
+        double minCost = DBL_MAX, totalQuality = 0;
+        vector<pair<double, int>> wageQuality;
+
+        for (int i = 0; i < n; i++)
+        {
+            wageQuality.emplace_back(1.0 * wage[i]/quality[i], quality[i]);
+        } 
+
+        sort(wageQuality.begin(), wageQuality.end());
+
+        priority_queue<double> highQualityWorkers;
+
+        for (int i = 0; i < n; i++)
+        {
+            totalQuality += wageQuality[i].second;
+            highQualityWorkers.emplace(wageQuality[i].second);
+
+            if (highQualityWorkers.size() > k)
+            {
+                totalQuality -= highQualityWorkers.top();
+                highQualityWorkers.pop();
+            }
+
+            if (highQualityWorkers.size() == k)
+            {
+                minCost = min(minCost, wageQuality[i].first * totalQuality);
+            }
+        }
+
+        return minCost;
+    }
+};
