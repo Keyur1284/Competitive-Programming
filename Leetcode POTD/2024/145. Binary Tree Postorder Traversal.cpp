9c1a821b8ab38b545ddca97e972e1f31c51d2125
Govrani Keyur <202101498@daiict.ac.in>
2024-08-25 12:25:54 +0530

    Solved in best optimized way

---
+++ Leetcode POTD/2024/145. Binary Tree Postorder Traversal.cpp
@@ -0,0 +1,79 @@
+// Problem Link :- https://leetcode.com/problems/binary-tree-postorder-traversal/
+
+// Solved by Recursive Approach
+// Time Complexity :- O(n)
+// Space Complexity :- O(n)
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    vector<int> ans;
+
+    void postOrder(TreeNode* root)
+    {
+        if (root == NULL)
+            return;
+
+        postOrder(root->left);
+        postOrder(root->right);
+        ans.emplace_back(root->val);
+    }
+
+    vector<int> postorderTraversal(TreeNode* root) {
+        
+        postOrder(root);
+
+        return ans;
+    }
+};
+
+
+// Solved by Iterative Approach
+
+class Solution {
+public:
+
+    vector<int> postorderTraversal(TreeNode* root) {
+        
+        vector<int> ans;
+
+        if (root == NULL)
+            return ans;
+
+        stack<TreeNode*> st1, st2;
+        st1.emplace(root);
+
+        while (!st1.empty())
+        {
+            TreeNode* node = st1.top();
+            st1.pop();
+            st2.emplace(node);
+
+            if (node->left)
+                st1.emplace(node->left);
+
+            if (node->right)
+                st1.emplace(node->right);
+        }
+
+        while (!st2.empty())
+        {
+            ans.emplace_back(st2.top()->val);
+            st2.pop();
+        }
+
+        return ans;
+    }
+};
