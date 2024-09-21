689b8f4e91a74d84e7c5a43bdc8156566d73a194
Govrani Keyur <202101498@daiict.ac.in>
2024-09-21 12:04:51 +0530

    Solved by DFS

---
+++ Leetcode POTD/2024/386. Lexicographical Numbers.cpp
@@ -0,0 +1,30 @@
+// Problem Link :- https://leetcode.com/problems/lexicographical-numbers/
+
+// Solved by DFS
+// Time Complexity :- O(n)
+// Space Complexity :- O(1)
+
+class Solution {
+public:
+
+    vector<int> order;
+
+    void DFS(int num, int n)
+    {
+        if (num > n)
+            return;
+
+        order.emplace_back(num);
+
+        for (int next = 0; next < 10; next++)
+            DFS(num * 10 + next, n);
+    }
+
+    vector<int> lexicalOrder(int n) {
+        
+        for (int start = 1; start <= min(n, 9); start++)
+            DFS(start, n);
+
+        return order;
+    }
+};
